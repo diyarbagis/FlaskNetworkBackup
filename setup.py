640294a5b8714b90149308227cdd6070ef73f7a8
d1b <50266008+diyarbagis@users.noreply.github.com>
2023-03-16 16:43:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FlaskNetworkBackup',
-    version='1.0.1',
+    version='1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['Flask', 'netmiko'],
@@ -23,7 +23,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Flask',
-        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Information Technology',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
